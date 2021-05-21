73818e8bf7a02949bb889a361b5883bc0e08cab8
Kirill <kirillkalinckin@yandex.ru>
2021-05-21 18:53:20 +0300

    сделал develop

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,17 @@ import { useEffect } from 'react';
 import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
 import Main from './Main';
 import Footer from './Footer';
+import Header from './Header';
+import AuthPopup from './AuthPopup';
 
 function App() {
   return (
     <div className='body'>
       <div className='page'>
+        <Header />
         <Main />
         <Footer />
+        <AuthPopup />
       </div>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+function Header () {
+  return (
+    <header className="header page__header">
+        <a href="./index.html" className="header__logo">наставники.про</a>
+        <nav className="navigation navigation_place_header">
+          <ul className="navigation__links navigation__links_place_header">
+            <li className="navigation__list">
+              <a href="./calendar.html" className="navigation__link navigation__link_place_header">календарь</a>
+            </li>
+            <li className="navigation__list">
+              <a href="./places.html" className="navigation__link navigation__link_place_header">куда пойти</a>
+            </li>
+            <li className="navigation__list">
+              <a href="#" className="navigation__link navigation__link_place_header">вопросы</a>
+            </li>
+            <li className="navigation__list">
+              <a href="#" className="navigation__link navigation__link_place_header">читать и смотреть</a>
+            </li>
+            <li className="navigation__list">
+              <a href="#" className="navigation__link navigation__link_place_header">права детей</a>
+            </li>
+            <li className="navigation__list">
+              <a href="#" className="navigation__link navigation__link_place_header">истории</a>
+            </li>
+          </ul>
+        </nav>
+        <form action="#" name="searcg" className="header__form-search display-none" method="POST">
+          <label className="header__field">
+            <input id="search-input" required name="search" minLength={2} maxLength={40} placeholder className="header__input" type="text" />
+            <span className="header__input-error" />
+          </label>
+          <div className="header__answer">
+            <p className="header__title">Причины подростковой агрессии</p>
+            <p className="header__category">статьи</p>
+          </div>
+          <div className="header__answer">
+            <p className="header__title">Агрессивное поведение детей-сирот</p>
+            <p className="header__category">видео</p>
+          </div>
+          <div className="header__answer">
+            <p className="header__title">Что делать если ваш младший агрессивно себя ведет, решил закрыть пару?</p>
+            <p className="header__category">книги</p>
+          </div>
+        </form>
+        <button type="button" className="header__search" />
+        <div className="header__divisor" />
+        <button type="button" className="header__account" />
+        <button type="button" className="header__menu" />
+      </header>
+  )
+}
+
+export default Header;
