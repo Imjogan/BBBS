f4c436240db31d03afc74553d2e0b853450945e4
Kirill <kirillkalinckin@yandex.ru>
2021-05-23 15:44:20 +0300

    Добавить ProtectedRoute

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import '../index.css';
+import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useEffect } from 'react';
 import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
 import Main from './Main/Main';
 import Footer from './Footer';
@@ -8,12 +8,14 @@ import Header from './Header';
 import AuthPopup from './AuthPopup';
 
 function App() {
+const [isLoggedIn, setIsLoggedIn] = React.useState(true)
+
   return (
     <div className='body'>
       <div className='page'>
         <Header />
         <main class='content page__content'>
-          <Main />
+          <Main isLoggedIn={isLoggedIn}/>
         </main>
           <Footer />
           <AuthPopup />
--- src/components/Main/GreetingInfo.js
+++ src/components/Main/GreetingInfo.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CalendarBlock from './CalendarBlock';
-import Article from '../Article';
+import Article from './Article';
 
 
 function GreetingInfo(props) {
-  const Calendar = false; // временное hard-значение
   return (
     <section className="two-columns two-columns_style_first">
-      {Calendar ? <CalendarBlock /> : <Article />}
+      {props.isLoggedIn ? <CalendarBlock /> : <Article />}
       <Link to="#">
         <article className="block-photo">
           <h2 className="block-photo__title">История Марины и Алины</h2>
