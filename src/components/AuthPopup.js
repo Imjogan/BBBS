a552c982c94a606992dc9c206ca306add72ab3ae
Marina Anisimova <anisimova.marina.n@gmail.com>
2021-05-25 00:01:10 +0300

    feature: добавить валидацию для формы с помощью хука

--- src/components/AuthPopup.js
+++ src/components/AuthPopup.js
@@ -1,6 +1,35 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
 function AuthPopup () {
+
+  // валидация формы
+  const { register, handleSubmit } = useForm();
+
+  const [red, setRed] = React.useState(false);
+
+  const isEmpty = (obj) => {
+    if (Object.keys(obj).length === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  const onSubmit = data => {
+    console.log(data); // поменять на запрос к серверу
+    setRed(false);
+  }; 
+
+  const onError = (errs) => {
+    console.log(errs);
+    if (!isEmpty(errs)) {
+      setRed(true);
+    } 
+  };
+
+
   return (
-    <div className="popup popup-sign-in">
+    <div className="popup popup-sign-in visible_block">
         <div className="popup__container">
           <button type="button" aria-label="close popup" className="popup__close" />
           <h2 className="popup__title">Вход</h2>
@@ -8,17 +37,29 @@ function AuthPopup () {
             Сёстры».</p>
           <p className="popup__message">Пожалуйста, введите логин и пароль из письма. Если вам не приходило письмо,
             свяжитесь с вашим куратором.</p>
-          <form action="#" name="sign-in" className="popup__form" method="POST">
+          <form action="#" name="sign-in" className="popup__form"  onSubmit={handleSubmit(onSubmit, onError)} noValidate>
             <label className="popup__field">
-              <input id="login-input" required name="login" minLength={2} maxLength={40} placeholder="Логин" className="popup__input" type="text" />
-              <span className="popup__input-error" />
+              <input id="login-input" {...register("login", 
+                { required: true, minLength: 2, maxLength: 40 })}
+                placeholder="Логин"                 
+                className={ red ? "popup__input popup__input_error" : "popup__input" }type="text" 
+                onChange={() => setRed(false)}
+              />
             </label>
             <label className="popup__field">
-              <input id="password-input" required name="password" minLength={2} maxLength={200} placeholder="Пароль" className="popup__input" type="password" />
-              <span className="popup__input-error" />
+              <input id="password-input" 
+                {...register("password", { required: true, minLength: 8, maxLength: 200 })}
+                placeholder="Пароль" 
+                className={ red ? "popup__input popup__input_error" : "popup__input" }
+                type="password" 
+                onChange={() => setRed(false)}
+              />
             </label>
             <p className="popup__help">Забыли пароль?</p>
-            <button aria-label="submit form" className="popup__button-submit" type="submit">Войти</button>
+            <button 
+              aria-label="submit form" className="popup__button-submit" 
+              type="submit"
+            >Войти</button>
           </form>
         </div>
       </div>
