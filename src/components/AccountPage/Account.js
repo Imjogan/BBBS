6ec967a119e092f64bc001797ef18fe1e89a2040
Sirhjkru <sirhjkru@gmail.com>
2021-06-05 16:16:04 +0300

    Сделал сортировку событий в лк

--- src/components/AccountPage/Account.js
+++ src/components/AccountPage/Account.js
@@ -1,48 +1,57 @@
 import React from "react";
 import Event from "./Event";
-import CurrentUserContext from '../../context/CurrentUserContext';
-import CurrentListOfEvents from '../../context/CurrentListOfEvents';
+import CurrentUserContext from "../../context/CurrentUserContext";
+import CurrentListOfEvents from "../../context/CurrentListOfEvents";
 import api from "../../utils/api";
+import { sortingArrayOrderByDate } from "../../utils/formatTime";
 
 function Account(props) {
   const userData = React.useContext(CurrentUserContext);
   const events = React.useContext(CurrentListOfEvents);
   const [city, setCity] = React.useState([]);
 
-
   React.useEffect(() => {
-    api.getCitiesList()
-      .then((res) => res.data.map((el)=> (
-        el.id === userData.city && setCity(el.name)
-      )))
+    api
+      .getCitiesList()
+      .then((res) =>
+        res.data.map((el) => el.id === userData.city && setCity(el.name))
+      );
   }, []);
 
   return (
     <>
       <section className="account">
         <div className="account__buttons">
-          <button type="button" className="account__button">{city}. Изменить ваш город</button>
-          <button type="button" onClick={props.signOut} className="account__button">Выйти</button>
-        </div>      
-        <p className="account__events-text">{events.length!== 0 ? "Вы записаны на мероприятия:" : "У вас нет записи на мероприятие"}
+          <button type="button" className="account__button">
+            {city}. Изменить ваш город
+          </button>
+          <button
+            type="button"
+            onClick={props.signOut}
+            className="account__button"
+          >
+            Выйти
+          </button>
+        </div>
+        <p className="account__events-text">
+          {events.length !== 0
+            ? "Вы записаны на мероприятия:"
+            : "У вас нет записи на мероприятие"}
         </p>
         <div className="account__events">
           <div className="account__scroll">
-            { events.length!== 0 
-              && events.map((event) => (
-                event.booked &&
-                <Event 
-                  key={event.id} 
-                  event={event}
-                  enroll={props.enroll}
-                />
-              ))
-            }
+            {events.length !== 0 &&
+              sortingArrayOrderByDate(events).map(
+                (event) =>
+                  event.booked && (
+                    <Event key={event.id} event={event} enroll={props.enroll} />
+                  )
+              )}
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
 
 export default Account;
