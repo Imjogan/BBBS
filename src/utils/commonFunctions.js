27690e9db3699ec9e7f04cab7cf40e4bd3e37243
Kirill <kirillkalinckin@yandex.ru>
2021-06-18 02:25:44 +0300

    правки по ревью

--- src/utils/commonFunctions.js
+++ src/utils/commonFunctions.js
@@ -3,7 +3,7 @@ function addPlus(array, obj) {
   if (array.length > 1) {
     if (array.indexOf(obj) !== array.length - 1) {
       return `${obj} + `
-    }  return obj
+    }  
   } return obj
 }
 
