a58846fc0c18ab0f4284c766c4eb538c970f45cd
Kirill <kirillkalinckin@yandex.ru>
2021-06-21 14:59:58 +0300

    обновить 404

--- src/components/Animation/Animation.jsx
+++ src/components/Animation/Animation.jsx
@@ -2,16 +2,16 @@ import { useEffect } from "react";
 import Lottie from "lottie-web";
 
 // eslint-disable-next-line react/prop-types
-function Animation({ animationData, containerClassname }) {
+function Animation({ animationData }) {
   useEffect(() => {
-    const container = document.querySelector(`.${containerClassname}`);
+    const container = document.querySelector(".popup-calendar__img");
     const options = {
       container,
       animationData,
-    };
+    }
     Lottie.loadAnimation(options);
   }, []);
-  return <div className={containerClassname} />;
+  return <div className="popup-calendar__img" />;
 }
 
 export default Animation;
