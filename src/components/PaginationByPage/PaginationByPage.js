66c6cf6c2c74b39ae3f59590e3e0459e9265fdf9
Mjogan <wow.mjogan.official@yandex.ru>
2021-07-10 20:40:05 +0300

    исправлен баг с пагинацией

--- src/components/PaginationByPage/PaginationByPage.js
+++ src/components/PaginationByPage/PaginationByPage.js
@@ -5,27 +5,16 @@ import PaginationButton from "../PaginationButton/PaginationButton";
 function PaginationByPage({ count }) {
   const [listBtn, setListBtn] = React.useState([]);
 
-  function renderPagesBtn(a) {
-    const list = [];
-    for (let i = 1; i <= a; i + 1) {
-      list.push(<PaginationButton key={i} pageNumber={i} />);
-    }
-    setListBtn(list);
-    console.log(list);
-  }
-
   useEffect(() => {
-    renderPagesBtn(count);
-  }, []);
-
-  useEffect(() => {
-    renderPagesBtn(count);
+    setListBtn([...Array(count).keys()].slice(1));
   }, [count]);
 
   return (
     <section className="select-page">
       <ul className="select-page__items">
-        {listBtn}
+        {listBtn.map((element, i) => (
+          <PaginationButton key={i} pageNumber={element} />
+        ))}
         {/* <PaginationButton pageNumber="1" isActiv={true} /> */}
         {/* <PaginationButton pageNumber="2" isActiv={false} /> */}
       </ul>
