cfb8f61e1121342207848fe765fd7e01e30420e4
Zhanna Anikanova <whiteredfoxx@gmail.com>
2021-07-11 01:50:05 +0300

    feat: компонента пагинации(без разделителя многоточием)

--- src/components/PaginationByPage/PaginationByPage.js
+++ src/components/PaginationByPage/PaginationByPage.js
@@ -2,23 +2,47 @@ import React, { useEffect } from "react";
 import "./PaginationByPage.css";
 import PaginationButton from "../PaginationButton/PaginationButton";
 
-function PaginationByPage({ count }) {
+function PaginationByPage({
+  count,
+  applyNextPagination,
+  currentPage,
+  applyPreviousPagination,
+}) {
   const [listBtn, setListBtn] = React.useState([]);
 
   useEffect(() => {
-    setListBtn([...Array(count).keys()].slice(1));
+    setListBtn([...Array(count + 1).keys()].slice(1));
   }, [count]);
 
   return (
     <section className="select-page">
+      {currentPage !== 1 && (
+        <button
+          onClick={applyPreviousPagination}
+          type="button"
+          aria-label="next-page"
+          className="button-previous"
+        />
+      )}
       <ul className="select-page__items">
-        {listBtn.map((element, i) => (
-          <PaginationButton key={i} pageNumber={element} />
+        {listBtn.map((i) => (
+          <PaginationButton
+            key={i}
+            pageNumber={i}
+            isActive={i === currentPage}
+          />
         ))}
-        {/* <PaginationButton pageNumber="1" isActiv={true} /> */}
-        {/* <PaginationButton pageNumber="2" isActiv={false} /> */}
+        {/* <PaginationButton pageNumber="..." /> */}
+        {/* <PaginationButton pageNumber={listBtn.length - 1} /> */}
       </ul>
-      <button type="button" aria-label="next-page" className="button-next" />
+      {currentPage !== count && (
+        <button
+          onClick={applyNextPagination}
+          type="button"
+          aria-label="next-page"
+          className="button-next"
+        />
+      )}
     </section>
   );
 }
