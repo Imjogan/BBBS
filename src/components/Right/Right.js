fef4181ffb87aad0bc83c2cdffd7879f6e73bcd8
Mjogan <wow.mjogan.official@yandex.ru>
2021-07-15 02:45:16 +0300

    реализована последовательность цветов и форм, реализована смена формы шапки статей

--- src/components/Right/Right.js
+++ src/components/Right/Right.js
@@ -1,14 +1,14 @@
 import './Right.css';
 import { Link } from 'react-router-dom';
-import { formArray } from '../../utils/constants';
+import { formArray, colorArray } from '../../utils/constants';
 
 const Right = ({ ...right }) => (
   <div className="guide-container__card">
     <div className="block-rights">
       <Link
         className={`block-rights__wrapper ${
-          formArray[Math.floor(Math.random() * formArray.length)]
-        } background-${right.color}`}
+          formArray[(right.id - 1) % formArray.length]
+        } ${colorArray[(right.id - 1) % colorArray.length]}`}
         to={`/right/${right.id}`}
       >
         <h2 className="block-rights__title">{right.title}</h2>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,25 +1,56 @@
 export const months = [
-  "Январь",
-  "Февраль",
-  "Март",
-  "Апрель",
-  "Май",
-  "Июнь",
-  "Июль",
-  "Август",
-  "Сентябрь",
-  "Октябрь",
-  "Ноябрь",
-  "Декабрь",
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь',
 ];
 export const day = [
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье',
 ];
 
-export const formArray = ['form-circle', 'form-semicircle', 'form-default'];
+export const formArray = [
+  'form-default',
+  'form-circle',
+  'form-semicircle',
+  'form-circle',
+  'form-semicircle',
+  'form-default',
+  'form-semicircle',
+  'form-default',
+  'form-circle',
+];
+
+export const colorArray = [
+  'background-orange',
+  'background-green',
+  'background-orange',
+  'background-red',
+  'background-purple',
+  'background-red',
+  'background-green',
+  'background-orange',
+  'background-green',
+  'background-purple',
+  'background-red',
+  'background-purple',
+];
+
+export const articleRightStyle = [
+  'flank-semicircle',
+  'bottom-semicircle',
+  'ellipse',
+];
